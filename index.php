c16873091892cada8a9b7218edc783655582d048
nicoliberios <ingedesistemas24@gmail.com>
2024-12-11 19:29:36 -0500

    feat: :art: This is a

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ echo "<h1>Host: $hostname</h1>";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Con Docker</title>
+    <title>Conr</title>
     <style>
         body {
             font-family: Arial, sans-serif;
