36bfd4790a8f00fcff956a857231a60a6102ad80
nicoliberios <ingedesistemas24@gmail.com>
2024-12-11 19:06:46 -0500

    feat: :art: This is a bwe

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Con Docker 18:59</title>
+    <title>Con Docker 18:70</title>
     <style>
         body {
             font-family: Arial, sans-serif;
